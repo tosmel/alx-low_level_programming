8fb1163e538f9b9e132e5f70c770dcb8f8982692
tosmel <83775651+tosmel@users.noreply.github.com>
2021-06-19 04:52:13 +0800

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 /**
- * main - print if the number is postive, zero, or negative
+ * main - Entry point
  *
- * Description: using the main function
- * this program prints "Programming is positive, zero, or negative
- * Return: 0
+ * Return: Always 0 (Success)
  */
 int main(void)
+
 {
-char c;
-for (c = '0'; c <= '9'; c++)
+int i; /*Decarling statement*/
+
+/*for count 0-9 with format ASCII*/
+for (i = 48 ; i <= 57; i++)
 {
-	putchar(c);
+putchar (i); /*print i*/
 }
-putchar('\n');
+
+putchar ('\n'); /*new line*/
+
 return (0);
+
 }
