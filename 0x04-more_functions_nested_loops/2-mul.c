81331744f9e2efee5afc18efae86bd8b962e3904
tosmel <83775651+tosmel@users.noreply.github.com>
2021-06-24 14:19:13 +0800

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,1 +1,12 @@
-#innvolve
+#include "holberton.h"
+
+/**
+ * mul - multiply 2 integers
+ * @a: int type number
+ * @b: int type number
+ * Return: result of multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
