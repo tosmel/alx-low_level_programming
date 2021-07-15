9b2467920e5b7b98550a04ac9d002253fdc84ba1
tosmel <tosmeltungsten@gmail.com>
2021-07-15 01:37:35 -0700

    malloc check

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,1 +1,18 @@
-#ghhhhhs
+#include <stdlib.h>
+
+/**
+ *  * malloc_checked - malloc memory and exit 98 on failure
+ *   *
+ *    * @b: size to allocate
+ *     *
+ *      * Return: pointer to allocated data
+ *       */
+void *malloc_checked(unsigned int b)
+{
+		void *a;
+
+			a = malloc(b);
+				if (!a)
+							exit(98);
+					return (a);
+}
