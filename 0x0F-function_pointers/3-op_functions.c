579377c0fff4bf2fffed7c963afda798b61d47b0
tosmel <83775651+tosmel@users.noreply.github.com>
2021-07-21 02:10:45 +0800

    3-op_functions

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,62 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
 /**
- * op_add - adds two numbers
- *
- * @a: one integer
- * @b: second integer
- *
- * Return: sum of integers
+ * op_add - 5 functions
+ * @a: input one
+ * @b: input two
+ * Return: 0
  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
-
 /**
- * op_sub - subtract two numbers
- *
- * @a: one integer
- * @b: second integer
- *
- * Return: a - b
+ * op_sub - 5 functions
+ * @a: input one
+ * @b: input two
+ * Return: 0
  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
-
 /**
- * op_mul - multiplies two numbers
- *
- * @a: one integer
- * @b: second integer
- *
- * Return: a * b
+ * op_mul - 5 functions
+ * @a: input one
+ * @b: input two
+ * Return: 0
  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
-
 /**
- * op_div - divides two numbers
- *
- * @a: one integer
- * @b: second integer
- *
- * Return: a / b
+ * op_div - 5 functions
+ * @a: input one
+ * @b: input two
+ * Return: 0
  */
 int op_div(int a, int b)
 {
 	return (a / b);
 }
-
 /**
- * op_mod - modulus two numbers
- *
- * @a: one integer
- * @b: second integer
- *
- * Return: a % b
+ * op_mod - 5 functions
+ * @a: input one
+ * @b: input two
+ * Return: 0
  */
 int op_mod(int a, int b)
 {
