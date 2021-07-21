366c51fd2c02179cffd6d6ca6dd4a8d02afc0fdb
tosmel <83775651+tosmel@users.noreply.github.com>
2021-07-22 07:52:13 +0800

    3-main

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,49 +1,40 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "3-calc.h"
+
 /**
- * main - get_op_func has operators correlated with
- * func signs and funcs from op_func
- * if not 4 arguments, return Error & exit 98
- * if op is null, return Error & exit 99
- * if div or mod 0, return Error & exit 100
- * run calc, input one, operator, input two = pointer res to get_op
- * @argc: arguments
- * @argv: double pointer to arguments
- * Return: 0
+ * main - arguments for struct
+ * @argc: numbers arguments
+ * @argv: string arguments
+ * Return: 0.
  */
+
 int main(int argc, char *argv[])
 {
-	int one, two, ans;
-	int (*res)(int, int);
-	char *get_op;
+	int i, j;
+	int (*d)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	one = atoi(argv[1]);
-	two = atoi(argv[3]);
-	get_op = argv[2];
-
-	/* added edge case if argv[2] was longer than 1 char*/
-	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
+	if (argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*get_op == '/' || *get_op == '%') && (*argv[3] == '0'))
-	{
-		printf("Error\n");
-		exit(100);
-	}
+	d = get_op_func(argv[2]);
+		if (d == NULL)
+		{
+			printf("Error\n");
+			exit(99);
+		}
 
-	res = get_op_func(get_op);
-	ans = res(one, two);
 
-	printf("%d\n", ans);
+	i = atoi(argv[1]);
+	j = atoi(argv[3]);
+
+	printf("%d\n", d(i, j));
+
 	return (0);
 }
